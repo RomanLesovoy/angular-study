8e2dc0421677073104f9987317ecd34a968a0538
RomanLesovoy <romadesire97@gmail.com>
2024-09-22 10:51:17 +0300

    fix: clear code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DirectNameDirective } from './direct-name.directive';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DirectNameDirective],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'lesson-1';
-  show = true;
+  
 }
