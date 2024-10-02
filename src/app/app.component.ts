b9c1b4d2ffdd21531435de86d85c17f99554e422
RomanLesovoy <romadesire97@gmail.com>
2024-10-02 13:37:57 +0300

    fix: cleared main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DirectNameDirective } from './direct-name.directive';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DirectNameDirective],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
